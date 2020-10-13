a1fd925f3902078245e0b62cfc71f05640c12e84
bjmulli1 <60275610+bjmulli1@users.noreply.github.com>
2020-10-13 17:22:54 -0400

    Update blogs.js

--- app_api/controllers/blogs.js
+++ app_api/controllers/blogs.js
@@ -6,4 +6,68 @@ var sendJSONresponse = function(res, status, content) {
   res.json(content);
 };
 
+
 // GET blog using id
+module.exports.blogsReadOne = function(req, res) {
+  console.log('Finding blog details', req.params);
+  if (req.params && req.params.blogid) {
+    Blog
+      .findById(req.params.blogid)
+      .exec(function(err, location) {
+        if (!location) {
+          sendJSONresponse(res, 404, {
+            "message": "blog id not found"
+          });
+          return;
+        } else if (err) {
+          console.log(err);
+          sendJSONresponse(res, 404, err);
+          return;
+        }
+        console.log(location);
+        sendJSONresponse(res, 200, location);
+      });
+  } else {
+    console.log('No blogid specified');
+    sendJSONresponse(res, 404, {
+      "message": "No blogid in request"
+    });
+  }
+};
+
+
+// GET a list of all blogs
+module.exports.blogsReadAll = function(req, res) {
+  console.log('Getting blogs list');
+  Blog
+      .find()
+      .exec(function(err, results) {
+        if (!results) {
+          sendJSONresponse(res, 404, {
+            "message": "no blogs found"
+          });
+          return;
+        } else if (err) {
+          console.log(err);
+          sendJSONresponse(res, 404, err);
+          return;
+        }
+        console.log(results);
+        sendJSONresponse(res, 200, buildBlogList(req, res, results));
+      }); 
+};
+
+var buildBlogList = function(req, res, results) {
+  var blogs = [];
+  results.forEach(function(obj) {
+    blogs.push({
+      _id: obj._id,
+      address: obj.address,
+      rating: obj.rating,
+      _id: obj._id
+    });
+  });
+  return locations;
+};
+
+
