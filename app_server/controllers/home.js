20f680bcd88b03b87d9140daef4e3fe868f3e9af
bjmulli1 <60275610+bjmulli1@users.noreply.github.com>
2020-10-13 18:09:08 -0400

    Update home.js

--- app_server/controllers/home.js
+++ app_server/controllers/home.js
@@ -1,3 +1,8 @@
+var request = require('request');
+var apiOptions = {
+	server : "https://localhost:80"
+};
+
 /*GET homepage*/
 module.exports.home=function(req,res){
 	res.render('home',{title:'Benjamin Mullins\' Blog Site'});
