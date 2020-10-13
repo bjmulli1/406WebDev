151f95391acc29e9565e4e4a30f2a8362dba3e91
bjmulli1 <60275610+bjmulli1@users.noreply.github.com>
2020-10-13 15:47:20 -0400

    Update index.js

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var ctrlBlogs = require('../controllers/blogs');
 
-/* Blog Request */ 
+// Blog Request 
 router.post('/blogs', ctrlBlogs.blogsCreate);
 router.get('/blogs:blogid', ctrlBlogs.blogsReadOne);
 router.put('/blogs:blogid', ctrlBlogs.blogsUpdateOne);
