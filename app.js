e664aac2a6d6e01d89208972b83c40e0e780f7ce
bjmulli1 <60275610+bjmulli1@users.noreply.github.com>
2020-10-15 15:44:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-require('./app_api/models/db');
 var logger = require('morgan');
+require('./app_api/models/db');
 
-var indexRouter = require('./app_server/routes/index');
+var routes = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var routesApi = require('./app_api/routes/index');
 
@@ -13,7 +13,7 @@ var app = express();
 
 app.locals.moment = require('moment');
 
-// view engine setup
+// engine setup
 app.set('views', path.join(__dirname, './app_server/views'));
 app.set('view engine', 'ejs');
 
@@ -21,16 +21,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
-app.use('/js',express.static(__dirname + '/node_modules/jquery/dist'));
-app.use('/css',express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-app.use('/css',express.static(__dirname + '/public/stylesheets'));
-app.use('/webfonts',express.static(__dirname + '/public/fonts/webfonts/'));
-app.use('/js', express.static(__dirname + '/node_modules/moment/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/css', express.static(__dirname + '/public/stylesheets'));
+app.use('/webfonts', express.static(__dirname + '/public/fonts/webfonts/'));
 
-
-app.use('/', indexRouter);
+app.use('/', routes);
 app.use('/users', usersRouter);
 app.use('/api', routesApi);
 
@@ -45,7 +44,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // render error page
   res.status(err.status || 500);
   res.render('error');
 });
