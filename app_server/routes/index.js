881b532e770407258e001ccab9a804d2c048508a
bjmulli1 <60275610+bjmulli1@users.noreply.github.com>
2020-10-15 14:41:07 -0400

    Update index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var ctrlHome = require('../controllers/home');
 var ctrlBlogPages = require('../controllers/blog');
 
-/* Setup routes to pages */
+/* Routes to pages */
 router.get('/', ctrlHome.home);
 router.get('/blogList', ctrlBlogPages.blogList);
 router.get('/blogAdd', ctrlBlogPages.blogAdd);
