26b07f7dfc7f7479af2bca83268396cc60039ecc
bjmulli1 <60275610+bjmulli1@users.noreply.github.com>
2020-10-15 15:17:03 -0400

    Update db.js

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,124 +1,51 @@
 var mongoose = require('mongoose');
-var Blog = mongoose.model('blogger');
+var dbURI = 'mongodb://blogger:blogger@localhost/blogger';
+var graefulShutdown;
 
-var sendJSONresponse = function(res, status, content){
-	res.status(status);
-	res.json(content);
-};
+mongoose.connect(dbURI);
 
-/* GET blog by id */
-module.exports.blogGetOne = function(req, res) {
-	console.log('Finding blog details', req.params);
-	if (req.params && req.params.blogid) {
-		Blog
-			.findById(req.params.blogid)
-			.exec(function(err, location) {
-				if (!location) {
-					sendJSONresponse(res, 404, {
-						"message": "blogid not found"
-					});
-					return;
-				} else if (err) {
-					console.log(err);
-					sendJSONresponse(res, 404, err);
-					return;
-				}
-				console.log(location);
-				sendJSONresponse(res, 200, location);
-			});
-	} else {
-		console.log('No blogid specified');
-		sendJSONresponse(res, 404, {
-			"message": "No blogid in request"
-		});
-	}
-};
+// Monitor and report when database is connected
+mongoose.connection.on('connected', function () {
+	console.log('Mongoose connected to ' + dbURI);
+});
 
-/* GET list of all blogs */
-module.exports.blogGetAll = function(req, res){
-	console.log('Getting blog list');
-	Blog
-		.find()
-		.exec(function(err, results){
-			if(!results){
-				sendJSONresponse(res, 404, {
-					"message": "no blogs found"	
-				});
-				return;
-			} else if(err){
-				console.log(err);
-				sendJSONresponse(res, 404, err);
-				return;
-			}
-			console.log(results);
-			sendJSONresponse(res, 200, buildBlogList(req, res, results));
-		});
-};
+// Monitor and report error connecting to database
+mongoose.connection.on('error',function (err) {
+	console.log('Mongoose connection error: ' + err);
+});
 
-var buildBlogList = function(req, res, results){
-	var blogs = [];
-	results.forEach(function(obj){
-		blogs.push({
-			_id: obj._id,
-			blogTitle: obj.blogTitle,
-			blogText: obj.blogText,
-			createdOn: obj.createdOn
-		});
+// Monitor and report when database is disconnected
+mongoose.connection.on('disconnected', function () {
+	console.log('Mongoose disconnected');
+});
+
+// Closes (disconnects) from Mongoose DB upon shutdown
+gracefulShutdown = function (msg, callback) {
+	mongoose.connection.close(function () {
+		console.log('Mongoose disconnected through ' + msg);
+		callback();
 	});
-	return blogs;
 };
 
-/* POST new blog */
-module.exports.blogCreate = function(req, res){
-	console.log(req.body);
-	Blog
-		.create({
-			blogTitle: req.body.blogTitle,
-			blogText: req.body.blogText,
-			createdOn: req.body.createdOn
-		}, function(err, location){
-			if(err){
-				console.log(err);
-				sendJSONresponse(res, 400, err);
-			} else{
-				console.log(location);
-				sendJSONresponse(res, 201, location);
-			}
-		}
-		);
-};
+// For nodemon restarts
+process.once('SIGUSR2', function () {
+	gracefulShutdown('nodemon restart', function () {
+		process.kill(process.pid, 'SIGUSR2');
+	});
+});
 
-/* Update one blog */
-module.exports.blogUpdateOne = function(req, res){
-	console.log("Updating a blog with id of " + req.params.blogid);
-	console.log(req.body);
-	Blog
-		.findOneAndUpdate(
-			{ _id: req.params.blogid},
-			{ $set: {"blogTitle": req.body.blogTitle, "blogText": req.body.blogText, "createdOn": req.body.createdOn}},
-			function(err, response){
-				if(err){
-					sendJSONresponse(res, 400, err);
-				} else{
-					sendJSONresponse(res, 201, response);
-				}
-			}
-		);
-};
+// For app termination
+process.on('SIGINT',function () {
+	gracefulShutdown('app termination',function () {
+		process.exit(0);
+	});
+});
 
-/* Delete one blog */
-module.exports.blogDeleteOne = function(req, res){
-	console.log("Deleting blog with id of " + req.param.blogid);
-	console.log(req.body);
-	Blog
-		.findByIdAndRemove(req.params.blogid)
-		.exec (
-			function(err, response){
-				if(err){
-					sendJSONresponse(res, 404, err);
-				} else{
-					sendJSONresponse(res, 204, null);
-				}
-			}
-		);
-};
+// For Heroku app termination
+process.on('SIGTERM',function () {
+	gracefulShutdown('Heroku app shutdown',function () {
+		process.exit(0);
+	});
+});
+
+require('./blogs');
