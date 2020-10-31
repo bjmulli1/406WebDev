31cedd98b424ebd0e4105a9ad0a35c736e733441
bjmulli1 <60275610+bjmulli1@users.noreply.github.com>
2020-10-31 13:18:42 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,35 +3,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
 require('./app_api/models/db');
-
-var routes = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
 var routesApi = require('./app_api/routes/index');
-
-var app = express();
 
 app.locals.moment = require('moment');
 
-// engine setup
-app.set('views', path.join(__dirname, './app_server/views'));
-app.set('view engine', 'ejs');
+var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
-app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 app.use('/css', express.static(__dirname + '/public/stylesheets'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js',express.static(__dirname + '/app_client/lib'));
+app.use('/js',express.static(__dirname + '/app_client'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); 
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); 
 app.use('/webfonts', express.static(__dirname + '/public/fonts/webfonts/'));
-
-app.use('/', routes);
-app.use('/users', usersRouter);
 app.use('/api', routesApi);
+app.use(function(req, res) {
+	res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,7 +36,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // sets locals
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
