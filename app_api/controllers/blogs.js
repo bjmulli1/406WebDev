d65fc55e608392406d5d00ccb9b3a16a6fc0b416
bjmulli1 <60275610+bjmulli1@users.noreply.github.com>
2020-11-14 12:50:49 -0500

    Update blogs.js

--- app_api/controllers/blogs.js
+++ app_api/controllers/blogs.js
@@ -6,7 +6,7 @@ var sendJSONresponse = function(res, status, content){
 	res.json(content);
 };
 
-/* GET blog using id */
+/* get blog using id */
 module.exports.blogGetOne = function(req, res) {
 	console.log('Finding blog details', req.params);
 	if (req.params && req.params.blogid) {
@@ -34,7 +34,7 @@ module.exports.blogGetOne = function(req, res) {
 	}
 };
 
-/* GET blogs list */
+/* get blogs list */
 module.exports.blogGetAll = function(req, res){
 	console.log('Getting blog list');
 	Blog
@@ -70,35 +70,36 @@ var buildBlogList = function(req, res, results){
 	return blogs;
 };
 
-/* POST new blog */
+/* POST blog */
 module.exports.blogCreate = function(req, res){
 	console.log(req.body);
 	Blog
 		.create({
 			blogTitle: req.body.blogTitle,
 			blogText: req.body.blogText,
-			createdOn: req.body.createdOn
-		}, function(err, location){
+			createdOn: req.body.createdOn,
+			authorName: req.body.authorName,
+			authorEmail: req.body.authorEmail
+		}, function(err, blog){
 			if(err){
 				console.log(err);
 				sendJSONresponse(res, 400, err);
 			} else{
-				console.log(location);
-				sendJSONresponse(res, 201, location);
+				console.log(blog);
+				sendJSONresponse(res, 201, blog);
 			}
 		}
 		);
 };
 
-/* Update blog */
+/* Update a blog */
 module.exports.blogUpdateOne = function(req, res){
 	console.log("Updating a blog with id of " + req.params.blogid);
 	console.log(req.body);
 	Blog
 		.findOneAndUpdate(
 			{ _id: req.params.blogid},
-			{ $set: {"blogTitle": req.body.blogTitle, "blogText": req.body.blogText, 
-				 "createdOn": req.body.createdOn, "authorName": req.body.authorName, "authorEmail": req.body.authorEmail}},
+			{ $set: {"blogTitle": req.body.blogTitle, "blogText": req.body.blogText, "createdOn": req.body.createdOn, "authorName": req.body.authorName, "authorEmail": req.body.authorEmail}},
 			function(err, response){
 				if(err){
 					sendJSONresponse(res, 400, err);
@@ -109,7 +110,7 @@ module.exports.blogUpdateOne = function(req, res){
 		);
 };
 
-/* Delete one blog */
+/* Delete a blog */
 module.exports.blogDeleteOne = function(req, res){
 	console.log("Deleting blog with id of " + req.param.blogid);
 	console.log(req.body);
@@ -124,5 +125,5 @@ module.exports.blogDeleteOne = function(req, res){
 				}
 			}
 		);
-};
+}
 
